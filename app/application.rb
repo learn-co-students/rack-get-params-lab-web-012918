ec782e7a1c53c132e41106b3ead18f19a8c6ad4f
alin684 <al684@cornell.edu>
2018-02-16 13:02:36 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,20 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart == []
+        resp.write "Your cart is empty."
+      else
+        @@cart.each {|thing| resp.write "#{thing}\n"}
+      end
+    elsif req.path.match(/add/)
+      got_item = req.params["item"]
+      if @@items.include?(got_item)
+        @@cart << got_item
+        resp.write "added #{got_item}"
+      else
+        resp.write "We don't have that item."
+      end
     else
       resp.write "Path Not Found"
     end
